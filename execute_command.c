94038d0d3254231d49039bc64ed147ae5922d890
Giv-08 <chutima.p8@gmail.com>
2024-08-19 05:36:40 +0000

    add all files 0.1

--- execute_command.c
+++ execute_command.c
@@ -1,1 +1,35 @@
-ffff
+#include "shell.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+/**
+ * execute_command - execute the command
+ * Return: nothing
+ **/
+
+extern char **environ;
+
+void execute_command(char *command)
+{
+        pid_t pid;
+        int status;
+        char *argv[] = {command, NULL};
+
+        pid = fork();
+        if (pid == 0)
+        {
+                if (execve(command, argv, environ) == -1)
+                {
+                        perror("Error with execve");
+                        exit(EXIT_FAILURE);
+                }
+        }
+        else
+        {
+                perror("Fork failed");
+                wait(&status);
+        }
+}
