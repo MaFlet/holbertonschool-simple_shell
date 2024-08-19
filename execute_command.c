be16f0f3fba6f036744a17709bbb29d47ecdc6e1
Giv-08 <chutima.p8@gmail.com>
2024-08-19 05:51:47 +0000

    fixed gcc

--- execute_command.c
+++ execute_command.c
@@ -4,21 +4,29 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * execute_command - execute the command
  * Return: nothing
  **/
-
 extern char **environ;
 
 void execute_command(char *command)
 {
         pid_t pid;
         int status;
-        char *argv[] = {command, NULL};
+        char *argv[2];
+	argv[0] = strdup(command);
+	argv[1] = NULL;
 
         pid = fork();
+	if (pid == -1)
+	{
+		perror("Fork failed");
+		free(argv[0]);
+		return;
+	}
         if (pid == 0)
         {
                 if (execve(command, argv, environ) == -1)
@@ -29,7 +37,9 @@ void execute_command(char *command)
         }
         else
         {
-                perror("Fork failed");
-                wait(&status);
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("Waitpid failed");
+		}
         }
 }
