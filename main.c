48c2cfb6f6b566324f108ceff080aca9e1704317
Giv-08 <chutima.p8@gmail.com>
2024-08-19 03:54:42 +0000

    added print_prompt

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "shell.h"
+
+int main(void)
+{
+	print_prompt();
+	// get_input();
+	// execute();
+	return (0);
+}
---
+++ print_prompt.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "shell.h"
+
+/**
+ * print_promp - print prompt on terminal
+ * Return: nothing
+ **/
+
+void print_prompt(void)
+{
+	printf("cisfun$ ");
+}
