c9786c651ca534f6557d7570788bb159e0f3ec38
Giv-08 <chutima.p8@gmail.com>
2024-08-20 11:08:43 +0000

    debug

--- execute_command.c
+++ execute_command.c
@@ -11,21 +11,21 @@
  * Return: nothing
  **/
 extern char **environ;
-
+/*
 void execute_command(char *command)
 {
 	char *token;
 	int i = 0;
-	/*char *command_path;*/
+	*char *command_path;
         pid_t pid;
         int status = 0;
         char *argv[64];
 	
-	/*if (_strcmp(command, "exit") == 0)
-	{
+	if (_strcmp(command, "exit") == 0)
+	  {
 		free(command);
 		exit(status);
-	}*/
+	}
 
 	token = strtok(command, " \t");
         while (token != NULL && i < 63)
@@ -34,14 +34,14 @@ void execute_command(char *command)
                 token = strtok(NULL, " \t");
         }
         argv[i] = NULL;
-	/*tokenize_command(command, argv);
+	tokenize_command(command, argv);
 	command_path = find_command_path(argv[0]);
 
 	if (command_path == NULL)
 	{
 		fprintf(stderr, "Command not found: %s\n", argv[0]);
 		return;
-	}*/
+	}
 
         pid = fork();
 	if (pid < 0)
@@ -63,11 +63,64 @@ void execute_command(char *command)
 		{
 			perror("Waitpid failed");
 		}
-		/*else if (WIFEXITED(status))
+		else if (WIFEXITED(status))
 		{
 			exit(WEXITSTATUS(status));
-		}*/
+		}
 	}
-	/*free(command_path);*/
-	/*free(command);*/
+	free(command_path);
+	free(command);
+}*/
+
+#include "shell.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * execute_command - execute the command
+ * Return: nothing
+ **/
+extern char **environ;
+
+void execute_command(char *command)
+{
+        int i = 0;
+        pid_t pid;
+        char *token;
+        int status;
+        char *argv[64];
+
+        token = strtok(command, " \t");
+        while (token != NULL && i < 63)
+        {
+                argv[i++] = token;
+                token = strtok(NULL, " \t");
+        }
+        argv[i] = NULL;
+
+        pid = fork();
+        if (pid < 0)
+        {
+                perror("fork failed");
+                exit(EXIT_FAILURE);
+        }
+        if (pid == 0)
+        {
+                if (execve(argv[0], argv, environ) == -1)
+                {
+                        perror("Error with execve");
+                        exit(EXIT_FAILURE);
+                }
+        }
+        else
+        {
+                if (waitpid(pid, &status, 0) == -1)
+                {
+                        perror("Waitpid failed");
+                }
+        }
 }
