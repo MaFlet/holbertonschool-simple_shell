50a1d3ad185077e98a634e535e481363013f69b4
Giv-08 <chutima.p8@gmail.com>
2024-08-21 10:37:19 +0000

    added free exit

--- execute_command.c
+++ execute_command.c
@@ -11,57 +11,7 @@
  * command: take char 'command' as parameter
  * Return: nothing
  **/
-/*
-void execute_command(char *command)
-{
-	char *command_path;
-        pid_t pid;
-        int status = 0;
-        char *argv[64];
-	
-	if (_strcmp(command, "exit") == 0)
-	{
-		free(command);
-		exit(status);
-	}
 
-	tokenize_command(command, argv);
-	command_path = find_command_path(argv[0]);
-
-	if (command_path == NULL)
-	{
-		fprintf(stderr, "Command not found: %s\n", argv[0]);
-		return;
-	}
-
-        pid = fork();
-	if (pid < 0)
-	{
-		perror("fork failed");
-		exit(EXIT_FAILURE);
-	}
-       	if (pid == 0)
-       	{
-               	if (execve(argv[0], argv, environ) == -1)
-               	{
-                       	perror("Error with execve");
-                       	exit(EXIT_FAILURE);
-               	}
-       	}
-       	else
-       	{
-		if (waitpid(pid, &status, 0) == -1)
-		{
-			perror("Waitpid failed");
-		}
-		else if (WIFEXITED(status))
-		{
-			exit(WEXITSTATUS(status));
-		}
-	}
-	free(command_path);
-	free(command);
-}*/
 extern char **environ;
 
 void print_env(void)
@@ -92,6 +42,7 @@ void execute_command(char *command)
 
 	if (argv[0] && _strncmp(_strtrim(argv[0]), "exit", 4) == 0 && argv[1] == NULL)
    	{
+		free(argv[0]);
 		exit(0);
 	}
 
