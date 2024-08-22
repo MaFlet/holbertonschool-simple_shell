cf5a03fe5f7b67d97bad576c5d5ce99c1fc2af8d
elxq95 <leongxiqern12@gmail.com>
2024-08-22 04:38:08 +0000

    read_command.c betty

--- read_command.c
+++ read_command.c
@@ -7,52 +7,44 @@
 /**
  * read_command - Reads a command from standard input.
  * @interactive: If non-zero, prompts the user with a shell prompt.
- *
- * Return: A pointer to the trimmed command string, or NULL if the input
- *         is empty or only contains whitespace.
+ * Return: A pointer to the trimmed command string.
  */
 
 char *read_command(int interactive)
 {
-        size_t bufsize = 0, len;
-        ssize_t input;
+	size_t bufsize = 0, len;
+	ssize_t input;
 
-        if (interactive)
-                printf("cisfun$ ");
-
-        input = getline(&command_buffer, &bufsize, stdin);
-        if (input == -1)
-        {
-                if (input == EOF)
-                {
-                        if (interactive)
-                        {
-                                printf("\n");
-                        }
-                        free(command_buffer);
-                        command_buffer = NULL;
-                        exit(EXIT_SUCCESS);
-                }
-                else
-                {
-                        perror("getline failed...");
-                        free(command_buffer);
-                        command_buffer = NULL;
-                        exit(EXIT_FAILURE);
-                }
-        }
-
-        len = _strcspn(command_buffer, "\n");
-        command_buffer[len] = '\0';
-        command_buffer = _strtrim(command_buffer);
-        if (command_buffer == NULL || *command_buffer == '\0')
-        {
-                free(command_buffer);
-                return NULL;
-        }
-        {
-                command_buffer[input - 1] = '\0';
-        }
-        command_buffer = _strtrim(command_buffer);
-        return(command_buffer);
+	if (interactive)
+		printf("cisfun$ ");
+	input = getline(&command_buffer, &bufsize, stdin);
+	if (input == -1)
+	{
+		if (input == EOF)
+		{
+			if (interactive)
+				printf("\n");
+			free(command_buffer);
+			command_buffer = NULL;
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("getline failed...");
+			free(command_buffer);
+			command_buffer = NULL;
+			exit(EXIT_FAILURE);
+		}
+	}
+	len = _strcspn(command_buffer, "\n");
+	command_buffer[len] = '\0';
+	command_buffer = _strtrim(command_buffer);
+	if (command_buffer == NULL || *command_buffer == '\0')
+	{
+		free(command_buffer);
+		return (NULL);
+	}
+	command_buffer[input - 1] = '\0';
+	command_buffer = _strtrim(command_buffer);
+	return (command_buffer);
 }
