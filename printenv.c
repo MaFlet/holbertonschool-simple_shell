c6cfbd16c6f29b7caf3d4a17685a65071f23cdb6
elxq95 <leongxiqern12@gmail.com>
2024-08-23 11:18:13 +0000

    update printenv.c betty

--- printenv.c
+++ printenv.c
@@ -2,13 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * print_env - Prints the current environment variables
+ *
+ * This function iterates through the `environ` array, which contains
+ * the environment variables as strings in the format "KEY=VALUE".
+ * Each environment variable is printed to the standard output.
+ *
+ * Return: void
+ */
+
 void print_env(void)
 {
-        char **env = environ;
+	char **env = environ;
 
-        while (*env)
-        {
-                printf("%s\n", *env);
-                env++;
-        }
+	while (*env)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
 }
